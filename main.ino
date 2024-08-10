bb35ae2db86d5f8a56f96d3b09ce099f9bcf58c5
Mir Muhammad Abidul Haq (Ahnaf) <76505613+Ahnaf-nub@users.noreply.github.com>
2024-08-10 19:16:59 +0600

    Update main.ino

--- main.ino
+++ main.ino
@@ -7,9 +7,11 @@
 #define DHTPIN 18
 #define button 16
 #define DHTTYPE DHT22
+bool state = 0;
 Servo servo;
 DHT dht(DHTPIN, DHTTYPE);
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
+RTC_DS1307 rtc;
 void setup() {
   Serial.begin(115200);
   pinMode(button, INPUT_PULLUP);
@@ -17,28 +19,40 @@ void setup() {
     Serial.println(F("SSD1306 allocation failed"));
     while (true);
   }
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
+  }
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running, let's set the time!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
   servo.attach(28);
   dht.begin();
 }
 void loop() {
-  if button == true {
+  DateTime now = rtc.now();
   float temperature = dht.readTemperature();
-    float humidity = dht.readHumidity();
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 10);
-    display.print("Temperature: ");
-    display.setCursor(0, 80);
-    display.print(temperature);
-    display.setCursor(10, 10);
-    display.print("Humidity: ");
-    display.setCursor(10, 80);
-    display.print(humidity);
-    display.display();
-    delay(1000);
-  }
-  else{
-    // gonna add later
-  }
+  float humidity = dht.readHumidity();
+  char buf[] = "DDD, MMM DD YYYY";
+  char buf1[] = "hh:mm";
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  display.print("Temperature: ");
+  display.setCursor(80, 10);
+  display.print(temperature);
+  display.setCursor(0, 20);
+  display.print("Humidity: ");
+  display.setCursor(80, 20);
+  display.print(humidity);
+  display.setCursor(20, 30);
+  display.print(now.toString(buf));
+  display.setCursor(0, 40);
+  display.print("Time: ");
+  display.setCursor(50, 40);
+  display.print(now.toString(buf1));
+  display.display();
 }
